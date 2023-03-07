6cfdedcb190f15066eacdabd7b8842ea79cfbc38
zhangyunfei <zhangyunfeimail@foxmail.com>
2023-03-07 14:01:21 +0800

    update

--- core/gorm.go
+++ core/gorm.go
@@ -1,19 +1,16 @@
 package core
 
 import (
-	"fmt"
 	"github.com/zhime/gin-vue/global"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 	"gorm.io/gorm/logger"
-	"log"
 	"time"
 )
 
 func InitGorm() *gorm.DB {
 	if global.Config.Mysql.Host == "" {
-		//global.LOG.Warn("未配置mysql")
-		log.Panicf("未配置mysql")
+		global.Log.Warnln("未配置mysql")
 		return nil
 	}
 	dsn := global.Config.Mysql.Dsn()
@@ -27,9 +24,8 @@ func InitGorm() *gorm.DB {
 		Logger: mysqlLogger,
 	})
 	if err != nil {
-		//global.LOG.Error(fmt.Errorf("[%s] mysql 连接失败", dsn))
-		log.Fatalf(fmt.Sprintf("[%s] mysql 连接失败", dsn))
-		//panic(err)
+		global.Log.Fatalf("[%s] mysql 连接失败", dsn)
+		panic(err)
 	}
 	sqlDB, _ := db.DB()
 	sqlDB.SetMaxIdleConns(10)
