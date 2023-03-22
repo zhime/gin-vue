99950621ba6db4435fd3605a696a00607fe7efd7
zhangyunfei <zhangyunfeimail@foxmail.com>
2023-03-22 11:43:01 +0800

    update

--- gorm_study/models/students.go
+++ gorm_study/models/students.go
@@ -1,7 +1,7 @@
 package models
 
 type Student struct {
-	ID   uint
-	Name string
-	Age  int
+	ID   uint   `gorm:"column:id;size:20"`
+	Name string `gorm:"column:name;size:16;comment:姓名"`
+	Age  int    `gorm:"column:age;size:3;comment:年龄"`
 }
