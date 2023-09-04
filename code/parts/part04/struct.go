0e8829daa750a23517e6dc10e2833187170982c0
zhime <931013000@qq.com>
2023-09-04 14:00:22 +0800

    update struct

--- code/parts/part04/struct.go
+++ code/parts/part04/struct.go
@@ -3,8 +3,14 @@ package main
 import "fmt"
 
 type Person struct {
-	name string
-	age  uint8
+	name   string
+	age    uint8
+	gender string
+}
+
+type Student struct {
+	person Person
+	class  string
 }
 
 func main() {
@@ -12,5 +18,15 @@ func main() {
 		name: "test",
 		age:  20,
 	}
+
+	s := Student{
+		person: Person{
+			name:   "dev",
+			age:    18,
+			gender: "男",
+		},
+		class: "一班",
+	}
 	fmt.Println(p.name, p.age)
+	fmt.Println(s.person.name, s.class)
 }
