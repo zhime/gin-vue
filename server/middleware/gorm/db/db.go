c842cf690072567093b7b66c487161f86a7cf156
zhime <931013000@qq.com>
2024-03-25 17:24:29 +0800

    update gorm

--- server/middleware/gorm/db/db.go
+++ server/middleware/gorm/db/db.go
@@ -26,13 +26,22 @@ func Init() {
 	//db := "user:pass@tcp(127.0.0.1:3306)/dbname?charset=utf8mb4&parseTime=True&loc=Local"
 	dsn := username + ":" + password + "@tcp(" + host + ":" + strconv.Itoa(port) + ")/" + dbName + "?" + config
 
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
-		//NamingStrategy: schema.NamingStrategy{
-		//	TablePrefix:   "gorm_", // 表名前缀
-		//	SingularTable: true,    // 单数表名
-		//},
-		//Logger: mysqlLogger,  // 日志
-	})
+	db, err := gorm.Open(mysql.New(mysql.Config{
+		DSN:                       dsn,   // DSN data source name
+		DefaultStringSize:         256,   // string 类型字段的默认长度
+		DisableDatetimePrecision:  true,  // 禁用 datetime 精度，MySQL 5.6 之前的数据库不支持
+		DontSupportRenameIndex:    true,  // 重命名索引时采用删除并新建的方式，MySQL 5.7 之前的数据库和 MariaDB 不支持重命名索引
+		DontSupportRenameColumn:   true,  // 用 `change` 重命名列，MySQL 8 之前的数据库和 MariaDB 不支持重命名列
+		SkipInitializeWithVersion: false, // 根据当前 MySQL 版本自动配置
+	}), &gorm.Config{})
+
+	//db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
+	//	//NamingStrategy: schema.NamingStrategy{
+	//	//	TablePrefix:   "gorm_", // 表名前缀
+	//	//	SingularTable: true,    // 单数表名
+	//	//},
+	//	//Logger: mysqlLogger,  // 日志
+	//})
 	if err != nil {
 		panic("数据库连接失败，" + err.Error())
 	}
