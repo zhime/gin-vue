5dc1c2190624651fe9cc646a493c2257b00f77a2
zhime <931013000@qq.com>
2024-03-27 17:41:35 +0800

    test

--- server/middleware/test/main.go
+++ server/middleware/test/main.go
@@ -2,6 +2,21 @@ package main
 
 import "fmt"
 
+type User struct {
+	name string
+}
+
+type Account struct {
+	User
+}
+
 func main() {
-	fmt.Println("test")
+	a := Account{
+		//name: "account",
+		User: User{
+			name: "user",
+		},
+	}
+	fmt.Println(a.name)
+	fmt.Println(a.User.name)
 }
