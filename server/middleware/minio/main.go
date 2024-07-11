aff267dbb531b623931677108cc6dd8f35d2e763
zhime <931013000@qq.com>
2024-07-11 14:07:28 +0800

    minio

---
+++ server/middleware/minio/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"context"
+	"github.com/minio/minio-go/v7"
+	"log"
+
+	"github.com/minio/minio-go/v7/pkg/credentials"
+)
+
+func main() {
+	ctx := context.Background()
+	endpoint := "192.168.34.33:9000"
+	accessKeyID := "XRuvYO7rCERtgTF8oARm"
+	secretAccessKey := "WcEq9AqzY5ltUDgMCkhqeTi1L49kp896EV5ghm2i"
+	useSSL := false
+
+	// Initialize minio client object.
+	minioClient, err := minio.New(endpoint, &minio.Options{
+		Creds:  credentials.NewStaticV4(accessKeyID, secretAccessKey, ""),
+		Secure: useSSL,
+	})
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	// Make a new bucket called testbucket.
+	bucketName := "netopstec"
+	location := ""
+
+	err = minioClient.MakeBucket(ctx, bucketName, minio.MakeBucketOptions{Region: location})
+	if err != nil {
+		// Check to see if we already own this bucket (which happens if you run this twice)
+		exists, errBucketExists := minioClient.BucketExists(ctx, bucketName)
+		if errBucketExists == nil && exists {
+			log.Printf("We already own %s\n", bucketName)
+		} else {
+			log.Fatalln(err)
+		}
+	} else {
+		log.Printf("Successfully created %s\n", bucketName)
+	}
+
+	// Upload the test file
+	// Change the value of filePath if the file is in another location
+	objectName := "ecs.png"
+	filePath := "C:\\Users\\ecs.png"
+	contentType := "image/png"
+
+	// Upload the test file with FPutObject
+	info, err := minioClient.FPutObject(ctx, bucketName, objectName, filePath, minio.PutObjectOptions{ContentType: contentType})
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	log.Printf("Successfully uploaded %s of size %d\n", objectName, info.Size)
+}
